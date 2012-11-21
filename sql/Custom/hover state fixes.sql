830d0cfa29fbe0611f66274b78b18194b35c4760
Terabyte <flowershendrik@ymail.com>
2012-11-21 03:23:50 -0800

    Set two NPCs to hover state.

---
+++ sql/Custom/hover state fixes.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_addon` SET  `bytes1` =  '33554432' WHERE  `guid` =202602; -- Vyragosa
+UPDATE `creature_template_addon` SET  `bytes1` =  '33554432' WHERE  `entry` =33687; -- Chillmaw
